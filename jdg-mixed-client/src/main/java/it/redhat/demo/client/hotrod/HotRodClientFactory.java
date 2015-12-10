0ec52aa0abf25e55594238eae6fc19f1fca498e5
francesco <francesco@localhost.localdomain>
2015-12-10 22:20:05 +0100

    fixed error

--- jdg-mixed-client/src/main/java/it/redhat/demo/client/hotrod/HotRodClientFactory.java
+++ jdg-mixed-client/src/main/java/it/redhat/demo/client/hotrod/HotRodClientFactory.java
@@ -27,12 +27,13 @@ public class HotRodClientFactory implements HotRodFactory {
     private static final Logger LOGGER = LoggerFactory.getLogger(HotRodClientFactory.class);
 
     private static final int DEFAULT_HOTROD_PORT = 11222;
-  RemoteCacheManager cacheManager = null;
+    RemoteCacheManager cacheManager = null;
+
     @Produces
     @ApplicationScoped
     @HotRodClient
     public RemoteCacheManager getCacheContainer() {
-      
+
         // Retrieve Infinispan config file.
         try {
             ProtoStreamMarshaller mm = new ProtoStreamMarshaller();
@@ -42,44 +43,44 @@ public class HotRodClientFactory implements HotRodFactory {
                     .host("127.0.0.1")
                     .port(DEFAULT_HOTROD_PORT).marshaller(mm);
 
-            cacheManager = new RemoteCacheManager(builder.build()); 
-            
-           SerializationContext serCtx = ProtoStreamMarshaller
-               .getSerializationContext(cacheManager);
-       configureProtobufMarshaller(serCtx);
+            cacheManager = new RemoteCacheManager(builder.build());
 
+            SerializationContext serCtx = ProtoStreamMarshaller
+                    .getSerializationContext(cacheManager);
+            configureProtobufMarshaller(serCtx);
 
         } catch (Exception ioe) {
             throw new RuntimeException("Error Connecting to Infinispan", ioe);
         }
         return cacheManager;
     }
-  private void configureProtobufMarshaller(SerializationContext serCtx) {
+
+    private void configureProtobufMarshaller(SerializationContext serCtx) {
        // generate and register a Protobuf schema and marshallers based
-       // on annotated classes
-       ProtoSchemaBuilder protoSchemaBuilder = new ProtoSchemaBuilder();
-       String generatedSchema;
-       try {
+        // on annotated classes
+        ProtoSchemaBuilder protoSchemaBuilder = new ProtoSchemaBuilder();
+        String generatedSchema;
+        try {
            // generate the 'twb.proto' schema file based on the annotations on
-           // passed classes and register it with the SerializationContext of the
-           // client
-           generatedSchema = protoSchemaBuilder.fileName("twb.proto")
-                   .packageName("it.redhat.demo.model")
-                   .addClass(Pojo.class)
-                   .build(serCtx);
+            // passed classes and register it with the SerializationContext of the
+            // client
+            generatedSchema = protoSchemaBuilder.fileName("twb.proto")
+                    .packageName("it.redhat.demo.model")
+                    .addClass(Pojo.class)
+                    .build(serCtx);
 
-           // register the schemas with the server too
-           RemoteCache<String, String> metadataCache = cacheManager
-                   .getCache(ProtobufMetadataManagerConstants.PROTOBUF_METADATA_CACHE_NAME);
-           metadataCache.put("twb.proto", generatedSchema);
+            // register the schemas with the server too
+            RemoteCache<String, String> metadataCache = cacheManager
+                    .getCache(ProtobufMetadataManagerConstants.PROTOBUF_METADATA_CACHE_NAME);
+            metadataCache.put("twb.proto", generatedSchema);
 
-           String errors = metadataCache.get(ProtobufMetadataManagerConstants.ERRORS_KEY_SUFFIX);
-           if (errors != null) {
-               throw new IllegalStateException("Some Protobuf schema files contain errors:\n"+ errors);
-           }
+            String errors = metadataCache.get(ProtobufMetadataManagerConstants.ERRORS_KEY_SUFFIX);
+            if (errors != null) {
+                throw new IllegalStateException("Some Protobuf schema files contain errors:\n" + errors);
+            }
 
-       } catch ( Exception e) {
-           throw new IllegalStateException( "An error occurred initializing HotRod protobuf marshaller:", e);
-       }
-   }
+        } catch (Exception e) {
+            throw new IllegalStateException("An error occurred initializing HotRod protobuf marshaller:", e);
+        }
+    }
 }

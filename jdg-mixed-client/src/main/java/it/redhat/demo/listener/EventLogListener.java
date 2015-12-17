8711f12df4014246a6714eb9dd202128ae2fa17a
Fabio Marinelli <f.marinelli@gmail.com>
2015-12-17 12:55:20 +0100

    fixed #1

--- jdg-mixed-client/src/main/java/it/redhat/demo/listener/EventLogListener.java
+++ jdg-mixed-client/src/main/java/it/redhat/demo/listener/EventLogListener.java
@@ -1,21 +1,28 @@
 package it.redhat.demo.listener;
 
-import org.infinispan.client.hotrod.annotation.*;
-import org.infinispan.client.hotrod.event.*;
+import org.infinispan.client.hotrod.annotation.ClientCacheEntryCreated;
+import org.infinispan.client.hotrod.annotation.ClientCacheEntryModified;
+import org.infinispan.client.hotrod.annotation.ClientCacheEntryRemoved;
+import org.infinispan.client.hotrod.annotation.ClientListener;
+import org.infinispan.client.hotrod.event.ClientCacheEntryCreatedEvent;
+import org.infinispan.client.hotrod.event.ClientCacheEntryModifiedEvent;
+import org.infinispan.client.hotrod.event.ClientCacheEntryRemovedEvent;
 
-@ClientListener
+@ClientListener(includeCurrentState = true)
 public class EventLogListener {
 
-  @ClientCacheEntryModified
-  @ClientCacheEntryRemoved
-    
-  @ClientCacheEntryCreated
-  public void entryAdded(ClientEvent event) {
-    
-    ClientCacheEntryCreatedEvent e = (ClientCacheEntryCreatedEvent)event;
-    System.out.println("[ClientListener] Key added " +e.getKey());
-  }
+    @ClientCacheEntryCreated
+    public void entryCreated(ClientCacheEntryCreatedEvent event) {
+        System.out.println("[ClientListener] Key added " + event.getKey() + " event[" + event + "]");
+    }
 
+    @ClientCacheEntryRemoved
+    public void entryRemoved(ClientCacheEntryRemovedEvent event) {
+        System.out.println("[ClientListener] Key removed " + event.getKey() + " event[" + event + "]");
+    }
 
-
+    @ClientCacheEntryModified
+    public void entryModified(ClientCacheEntryModifiedEvent event) {
+        System.out.println("[ClientListener] Key modified " + event.getKey() + " event[" + event + "]");
+    }
 }

f39f8867fc08157e08186499453a86f770772f36
Fabio Marinelli <f.marinelli@gmail.com>
2015-12-17 12:55:55 +0100

    refactoring client

--- jdg-mixed-client/src/main/java/it/redhat/demo/client/hotrod/HotRod6ClientManager.java
+++ jdg-mixed-client/src/main/java/it/redhat/demo/client/hotrod/HotRod6ClientManager.java
@@ -1,56 +1,52 @@
 package it.redhat.demo.client.hotrod;
 
+import it.redhat.demo.client.ProtocolServerManager;
 import it.redhat.demo.listener.EventLogListener;
-import javax.enterprise.context.ApplicationScoped;
-import javax.inject.Inject;
 import org.infinispan.client.hotrod.RemoteCache;
 import org.infinispan.client.hotrod.RemoteCacheManager;
- import it.redhat.demo.client.ProtocolServerManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
 
 @ApplicationScoped
 @HotRodClient
 public class HotRod6ClientManager implements ProtocolServerManager {
-	public static final Logger LOGGER = LoggerFactory.getLogger(HotRod6ClientManager.class);
+    public static final Logger LOGGER = LoggerFactory.getLogger(HotRod6ClientManager.class);
 
-	private static final int DEFAULT_HOTROD_PORT = 11222;
+    @Inject
+    private RemoteCacheManager cacheManager;
 
-	private static final int DEFAULT_WORKER_THREADS = 160;
+    @Override
+    public void bootstrapProtocolClient() {
+        RemoteCache<String, Object> cache = null;
 
-	private static final String bindAddress = "127.0.0.1";       
-       
-	@Inject
-	private RemoteCacheManager cacheManager;
+        boolean done = false;
+        try {
 
-	@Override
-	public void bootstrapProtocolClient() {
-            RemoteCache<String, Object> cache=null;
+            cache = cacheManager.getCache("MyCoolCache");
+            cache.addClientListener(new EventLogListener());
+            System.out.println("Added Listener");
 
-        boolean done = false;    
-  	try {
-      	
-        cache = cacheManager.getCache("MyCoolCache");
-        cache.addClientListener(new EventLogListener());
-        System.out.println("Added Listener");
+            done = true;
+        } catch (Exception e) {
+            String message = "Failed to start the HotRodServer.";
+            LOGGER.error(message, e);
+            throw new RuntimeException(message, e);
+        } finally {
+            if (!done) {
+                stop();
+            }
+        }
+    }
 
-			done = true;
-		} catch (Exception e) {
-			String message = "Failed to start the HotRodServer.";
-			LOGGER.error(message, e);
-			throw new RuntimeException(message, e);
-		} finally {
-			if (!done) {
-				stop();
-			}
-		}
-	}
- 
-	@Override
-	public synchronized void stop() {
-		if (cacheManager != null)
-                    cacheManager.stop();
-	}
+    @Override
+    public synchronized void stop() {
+        if (cacheManager != null) {
+            cacheManager.stop();
+        }
+    }
 
-  
+
 }
--- jdg-mixed-client/src/main/java/it/redhat/demo/web/listener/JdgStartListener.java
+++ jdg-mixed-client/src/main/java/it/redhat/demo/web/listener/JdgStartListener.java
@@ -1,60 +1,57 @@
 package it.redhat.demo.web.listener;
 
+import it.redhat.demo.client.ProtocolServerManager;
+import it.redhat.demo.client.hotrod.HotRodClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.inject.Inject;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
 
- import it.redhat.demo.client.ProtocolServerManager;
 //import org.jboss.ddoyle.infinispan.demo.server.hotrod.HotRod;
-import it.redhat.demo.client.hotrod.HotRodClient;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * Application Lifecycle Listener implementation class CacheStartListener
- * 
  */
 
 @WebListener
 public class JdgStartListener implements ServletContextListener {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(JdgStartListener.class);
-	
-	//@Inject
-	//private ApplicationCacheManager cacheManager;
-	
-	@Inject
-	@HotRodClient
-	private ProtocolServerManager protocolServerManager;
-	
-	//@Inject
-	//private CacheStatisticsMBean cacheStatistics;
-	
-	//TODO: Make this configurable or remove this option.
-	private boolean startCachesOnStartup = true;
-	
-	/**
-	 * Default constructor.
-	 */
-	public JdgStartListener() {
-	}
+    private static final Logger LOGGER = LoggerFactory.getLogger(JdgStartListener.class);
 
-	/**
-	 * @see ServletContextListener#contextInitialized(ServletContextEvent)
-	 */
-	public void contextInitialized(ServletContextEvent scEvent) {
-		//Fire up the Hotrod Listener
-		protocolServerManager.bootstrapProtocolClient();
-	}
-	
+    @Inject
+    @HotRodClient
+    private ProtocolServerManager protocolServerManager;
 
-	/**
-	 * @see ServletContextListener#contextDestroyed(ServletContextEvent)
-	 */
-	public void contextDestroyed(ServletContextEvent arg0) {
-		LOGGER.info("Stopping the HotRod server.");
-		protocolServerManager.stop();
-	}
+    //@Inject
+    //private CacheStatisticsMBean cacheStatistics;
+
+    //TODO: Make this configurable or remove this option.
+    private boolean startCachesOnStartup = true;
+
+    /**
+     * Default constructor.
+     */
+    public JdgStartListener() {
+    }
+
+    /**
+     * @see ServletContextListener#contextInitialized(ServletContextEvent)
+     */
+    public void contextInitialized(ServletContextEvent scEvent) {
+        //Fire up the Hotrod Listener
+        protocolServerManager.bootstrapProtocolClient();
+    }
+
+
+    /**
+     * @see ServletContextListener#contextDestroyed(ServletContextEvent)
+     */
+    public void contextDestroyed(ServletContextEvent arg0) {
+        LOGGER.info("Stopping the HotRod server.");
+        protocolServerManager.stop();
+    }
 
 }
